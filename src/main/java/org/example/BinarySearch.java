0e9be77566f45ef78b5980bacd6aa9fa0943804c
Kotikov <11deadsluts@gmail.com>
2023-09-24 11:22:12 +0300

    Done!

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -1,2 +1,28 @@
-package org.example;public class BinarySearch {
+package org.example;
+
+public class BinarySearch {
+
+    private int binarySearch(int[] sortedArray, int valueToFind, int first, int last) {
+        int i = -1;
+
+        while (first <= last) {
+            int mid = first + (last - first) / 2;
+            if (sortedArray[mid] < valueToFind) {
+                first = mid + 1;
+            } else if (sortedArray[mid] > valueToFind) {
+                last = mid - 1;
+            } else if (sortedArray[mid] == valueToFind) {
+                i = mid;
+                break;
+            }
+        }
+        if (i == -1) {
+            System.out.println("There's no any valid number");
+        }
+        return i;
+    }
+
+    public int findIndex(int[] sortedArray, int valueToFind) {
+        return binarySearch(sortedArray, valueToFind, 0, sortedArray.length - 1);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {1, 2, 4, 5, 6, 7, 8, 9, 10};
+        int valueToFind = -1;
+        BinarySearch iCanReturnYourValueToFind = new BinarySearch();
+
+        int indexOfValue = iCanReturnYourValueToFind.findIndex(array, valueToFind);
+        System.out.println("The index of [" + valueToFind + "] is " + indexOfValue );
     }
+
 }
